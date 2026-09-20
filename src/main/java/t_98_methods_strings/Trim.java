b2bcb768e4803e7995ef2477de82b7eb950e046e
Lubin Mendez <mendezlubin@yahoo.com>
2026-09-20 16:43:37 -0700

    String methods - trim

---
+++ src/main/java/t_98_methods_strings/Trim.java
@@ -0,0 +1,12 @@
+package t_98_methods_strings;
+
+public class Trim {
+    public static void main(String[] args) {
+        // Removes whitespace from the beginning and end
+
+        String note = " Message ";
+
+        System.out.println(note);
+        System.out.println(note.trim()); // "Message"
+    }
+}

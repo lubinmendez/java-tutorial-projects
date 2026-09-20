9ba75645f65ee27c2d892391f80399645b37a0a4
Lubin Mendez <mendezlubin@yahoo.com>
2026-09-20 16:39:22 -0700

    String methods - ToUpperCase -ToLowerCase

---
+++ src/main/java/t_98_methods_strings/ToLowerCase.java
@@ -0,0 +1,9 @@
+package t_98_methods_strings;
+
+public class ToLowerCase {
+    public static void main(String[] args) {
+
+        String name = "JOHN"; //
+        System.out.println(name.toLowerCase());
+    }
+}
---
+++ src/main/java/t_98_methods_strings/ToUpperCase.java
@@ -0,0 +1,9 @@
+package t_98_methods_strings;
+
+public class ToUpperCase {
+    public static void main(String[] args) {
+
+        String name = "John"; //
+        System.out.println(name.toUpperCase());
+    }
+}
